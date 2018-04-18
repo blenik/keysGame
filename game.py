9e0d5df4504c0bbf9910ae26ff7fa5ee7a691c7f
mrynek <rynek@pl.sii.eu>
2018-04-18 15:14:32 +0200

    Summary
    Klucze nie generuja sie w scianach

--- game.py
+++ game.py
@@ -3,18 +3,45 @@
 #comment all yours code!
 
 #section 0: global
+import random
 from InterfaceCreator import InterfaceCreator
 PLANSZA_SIZE=22
 
 
+# section 4: randomization
+def init_keys(Plansza):
+    i = 0
+    while i < 3:
+        t = random.sample(range(1, 21), 3)
+        Plansza[1][t[i]] = "k"
+        i = i + 1
+
+    i = 0
+    j = 1
+    while 3 * j < 21:
+        while i < 3:
+            t = random.sample(range(1, 21), 3)
+            if Plansza[3 * j][t[0]] == "#" or Plansza[3 * j][t[1]] == "#" or Plansza[3 * j][t[2]] == "#":
+                i=i
+            else:
+                Plansza[3 * j][t[i]] = "k"
+                i=i+1
+
+        i = 0
+        j = j + 1
 
 
 #section 1: map
 
 FileBoard = open('Level_1_Template.txt', 'r')
 BoardGameArray = FileBoard.read().splitlines()
-FileBoard.close()
-for row in BoardGameArray:
+
+lista=list()
+
+for linia in BoardGameArray:
+    lista.append([c for _,c in enumerate(linia)])
+init_keys(lista)
+for row in lista:
     print(''.join([str(elem) for elem in row]))
 
 
@@ -25,10 +52,6 @@ for row in BoardGameArray:
 
 #section 3: moving
 
-
-
-
-#section 4: randomization
 
 
 
