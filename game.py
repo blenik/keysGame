284ac093a1bbbe64246f52c5e4c8b94b8c77f75e
zosiaosm <osmanskazofia@gmail.com>
2018-04-18 16:11:54 +0200

    Utworzenie funkcji

--- game.py
+++ game.py
@@ -7,23 +7,27 @@ from InterfaceCreator import InterfaceCreator
 PLANSZA_SIZE=22
 
 
-
-
 #section 1: map
 
-# odczytanie pliku z mapa
-file = open('Level_1_Template.txt', 'r')
-BoardGameList = file.read().splitlines()
-file.close()
+lista = list()
 
-# lista - jest macierza mapy
-lista=list()
-for line in BoardGameList:
-    lista.append([c for _,c in enumerate(line)])
+# funkcja odczytujaca plik z mapa i wpisanie jej do macierzy 'lista'
+def readingLevelsFromFileToArray(lista):
+    file = open('Level_1_Template.txt', 'r')
+    boardgame = file.read().splitlines()
+    file.close()
+    for line in boardgame:
+        lista.append([c for _, c in enumerate(line)])
+    return lista
 
-# wyswietlenie mapy
-for row in lista:
-    print(''.join([str(elem) for elem in row]))
+# funkcja wyswietlajaca mape
+def printingTheMap(map):
+    for row in map:
+        print(''.join([str(elem) for elem in row]))
+
+# wywolanie funkcji
+readingLevelsFromFileToArray(lista)
+printingTheMap(lista)
 
 
 
