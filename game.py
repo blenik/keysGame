453ccbfd126483e33d3a027f23840a7481c6f8ad
blenik <blenik@hotmail.com>
2018-04-18 09:41:22 +0200

    first draft

---
+++ game.py
@@ -0,0 +1,40 @@
+#the project is divided into sections. 
+#edit only your section!
+#comment all yours code!
+
+#section 0: global
+
+
+
+
+#section 1: map
+
+
+
+
+#section 2: fight
+
+
+
+
+#section 3: moving
+
+
+
+
+#section 4: randomization
+
+
+
+
+#section 5: rendering
+
+
+
+
+#section 6: interface
+
+
+
+
+
