2537997fbc64dcb1e9cb1bfe84dca06057fbac8f
Piotr <piotr88101@gmail.com>
2018-04-19 13:17:04 +0200

    movment
    aktualne zmiany
    -cls() clear
    -scorecard
    -player improvement
    -player movement

--- game.py
+++ game.py
@@ -7,26 +7,32 @@ from InterfaceCreator import InterfaceCreator
 from msvcrt import getch
 from movement_2 import player
 import movement_2
+import os
 PLANSZA_SIZE=22
-
-
+interfacePrinter=InterfaceCreator(PLANSZA_SIZE,"*") # tworzy instancje creatora interfejsu
 
 
 #section 1: map
 
-# odczytanie pliku z mapa
-file = open('Level_1_Template.txt', 'r')
-BoardGameList = file.read().splitlines()
-file.close()
+lista = list()
 
-# lista - jest macierza mapy
-lista=list()
-for line in BoardGameList:
-    lista.append([c for _,c in enumerate(line)])
+# funkcja odczytujaca plik z mapa i wpisanie jej do macierzy 'lista'
+def readingLevelsFromFileToArray(lista):
+    file = open('Level_1_Template.txt', 'r')
+    boardgame = file.read().splitlines()
+    file.close()
+    for line in boardgame:
+        lista.append([c for _, c in enumerate(line)])
+    return lista
 
-# wyswietlenie mapy
-for row in lista:
-    print(''.join([str(elem) for elem in row]))
+# funkcja wyswietlajaca mape
+def printingTheMap(map):
+    for row in map:
+        print(''.join([str(elem) for elem in row]))
+
+# wywolanie funkcji
+readingLevelsFromFileToArray(lista)
+printingTheMap(lista)
 
 
 
@@ -36,50 +42,59 @@ for row in lista:
 
 
 #section 3: moving
-gracz_1 = player(1, 21)
-gracz_2 = player(21, 21)
 
-while true:
-    #if key == 27:  # ESC
-       # break
+gracz_1 = player(1, 20)
+gracz_2 = player(20, 20)
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+while True:
+    zmiennaP1=False
+    zmiennaP2=False
     #elif key == 13:  # Enter
       #  print('selected')
     #elif key == 32:  # Space
      #   print('jump')
     #elif key == 224:  # Special keys (arrows, f keys, ins, del, etc.)
-       # key = ord(getch())
+    key = ord(getch())
     if key == 115:  # S move down
         print('down')
-        gracz_1.move_down(lista)
+        zmiennaP1=gracz_1.move_down(lista)
     elif key == 53:  # 5 move down
         print('down')
-        gracz_2.move_down_down(lista)
+        zmiennaP2 =gracz_2.move_down(lista)
 
     elif key == 119:  # W move up
         print('up')
-        gracz_1.move_up(lista)
+        zmiennaP1 =gracz_1.move_up(lista)
     elif key == 56:  # 8 move up
         print('up')
-        gracz_2.move_up(lista)
+        zmiennaP2 =gracz_2.move_up(lista)
 
     elif key == 97:  # A move Left
         print('left')
-        gracz_1.move_left(lista)
+        zmiennaP1 =gracz_1.move_left(lista)
     elif key == 52:  # A move Left
         print('left')
-        gracz_2.move_left(lista)
+        zmiennaP2 =gracz_2.move_left(lista)
 
     elif key == 100:  # D move Right
         print('right')
-        gracz_1.move.right(lista)
+        zmiennaP1 =gracz_1.move_right(lista)
     elif key == 54:  # 6 move Right
         print('right')
-        gracz_2.move.right(lista)
+        zmiennaP2 =gracz_2.move_right(lista)
+    if key == 27:  # ESC
+        break
 
-
-
-
-
+    if zmiennaP1:
+        interfacePrinter.points_increase(True)
+    if zmiennaP2:
+        interfacePrinter.points_increase(False)
+    cls()
+    printingTheMap(lista)
+    interfacePrinter.print_interface()
 
 #section 4: randomization
 
--- movement_2.py
+++ movement_2.py
@@ -7,39 +7,63 @@ class player(object):
 
     def __init__(self, pos_x, pos_y):
         self.x = pos_x
-        self.y = pos_x
+        self.y = pos_y
 
-        punkty = 0
-        wynik = 0
+        self.punkty = 0
+        self.wynik = 0
+    def przestaw(self, x,y):
+        self.x = x
+        self.y = y
 
-    def move_up(self, map):
-        if map[self.x -1][self.y ] == "k" :
-            self.x -= 1
+    def move_up(self, plansza):
+        if plansza[self.x -1][self.y ] == "k":
             self.punkty += 1
-        elif map[self.x -1][self.y ] == " " :
-            self.x -= 1
+            zwracam = True
+        else:
+            zwracam = False
 
-    def move_down(self, map):
-        if map[self.x -1][self.y ] == "k" :
+        if plansza[self.x -1][self.y ] == " " or plansza[self.x -1][self.y ] == "k":
+            plansza[self.x][self.y] = " "
+            plansza[self.x -1][self.y] ="p"
             self.x -= 1
+        return  zwracam
+
+    def move_down(self, plansza):
+        if plansza[self.x +1][self.y ] == "k":
             self.punkty += 1
-        if map[self.x +1] [self.y]  == " ":
+            zwracam = True
+        else:
+            zwracam = False
+
+        if plansza[self.x +1][self.y] == " " or plansza[self.x +1][self.y ] == "k":
+            plansza[self.x][self.y] = " "
+            plansza[self.x + 1][self.y] = "p"
             self.x += 1
+        return zwracam
 
-    def move_right(self, map):
-        if map[self.x -1][self.y ] == "k" :
-            self.x -= 1
+    def move_right(self, plansza):
+        if plansza[self.x][self.y +1] == "k":
             self.punkty += 1
-        if map[self.x ] [self.y +1] == " ":
+            zwracam = True
+        else:
+            zwracam = False
+        if plansza[self.x][self.y +1] == " " or plansza[self.x ][self.y +1] == "k":
+            plansza[self.x][self.y] = " "
+            plansza[self.x][self.y +1] = "p"
             self.y += 1
+        return zwracam
 
-    def move_left(self, map):
-        if map[self.x -1][self.y ] == "k" :
-            self.x -= 1
+    def move_left(self, plansza):
+        if plansza[self.x ][self.y -1] == "k":
             self.punkty += 1
-        if map[self.x ] [self.y -1] == " ":
+            zwracam = True
+        else:
+            zwracam = False
+        if plansza[self.x ][self.y -1] == " " or plansza[self.x ][self.y -1] == "k":
+            plansza[self.x][self.y] = " "
+            plansza[self.x][self.y -1] = "p"
             self.y -= 1
-
+        return zwracam
 
 
 
