f58cc54f89fad96d77bf79e420fedc1d5405f96c
zosiaosm <osmanskazofia@gmail.com>
2018-05-17 10:28:44 +0200

    Added partial unittest.

--- map_importer.py
+++ map_importer.py
@@ -3,24 +3,29 @@ import os
 import sys
 
 
-# funkcja odczytujaca plik z mapa i wpisanie jej do macierzy 'lista'
-def readLevelsFromFileToArray(lista, filename):
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            try:
-                lines = file.read().splitlines()
-                file.close()
-                for line in lines:
-                    lista.append([c for _, c in enumerate(line)])
-            except IOError:
-                print("Could not read file:", filename)
-                sys.exit()
-    else:
-        print("There is not any file with map!")
-    return lista
+class MapImporter(object):
 
+# funkcja odczytujaca plik z mapa i wpisanie jej do macierzy 'list'
+    def readLevelsFromFileToArray(self, list, filename):
+        if os.path.exists(filename):
+            with open(filename, 'r') as fileStream:
+                try:
+                    self.readLinesFromFileStream(list, fileStream)
+                    fileStream.close()
+                except IOError:
+                    print("Could not read file:", filename)
+                    sys.exit()
+        else:
+            print("There is not any file with map!")
+        return list
 
-# funkcja wyswietlajaca mape
-def printTheMap(map):
-    for row in map:
-        print(''.join([str(elem) for elem in row]))
+    def readLinesFromFileStream(self, list, fileStream):
+        lines = fileStream.read().splitlines()
+        for line in lines:
+            list.append([c for _, c in enumerate(line)])
+        return list
+
+    # funkcja wyswietlajaca mape
+    def printTheMap(self, map):
+        for row in map:
+            print(''.join([str(elem) for elem in row]))
